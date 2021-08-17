8fc1f15b31b6f1a9e556b588bf2dbb1743950060
1BrandonVanZyl1 <89069026+1BrandonVanZyl1@users.noreply.github.com>
2021-08-17 14:05:40 +0200

    added file

---
+++ LMS.cs
@@ -0,0 +1,14 @@
+using system;
+
+namespace LMS{
+    class LMS{
+        static void Main(string[] args)
+        {
+            //this is the start
+
+            Console.WriteLine("Hello World");
+        }
+    }
+ 
+
+}
